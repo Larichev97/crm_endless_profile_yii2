698e4bb620ceffa94f2b6120508ef6e254b9a477
Denis Larichev <denis.larichev97@gmail.com>
2021-10-09 15:13:31 +0300

    V 1.11 added new design in Qr Profile.

--- views/qr/view.php
+++ views/qr/view.php
@@ -2,7 +2,6 @@
 
 use Carbon\Carbon;
 use yii\helpers\Html;
-use yii\widgets\DetailView;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Qr */
@@ -28,111 +27,202 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
     </div>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            [
-                'attribute' => 'qr_link',
-                'format' => 'raw',
-                'value' => function ($model) {
-                    if (!empty($model->qr_link)) {
-                        return '<div style="display:flex; align-items:center;">' . Html::img(Yii::getAlias('@web').'/images/qr-codes/'. $model->qr_link, ['width' => '200px', 'height' => '200px']) . '</div>';
-                    } else {
-                        return '<div style="display:flex; align-items:center;">' .
-                               '<a class="btn btn-warning" data-pjax="0" target="_blank" href="https://api.qrserver.com/v1/create-qr-code/?size=800x800&format=svg&data=http://crm_project/qr/profile?id='. $model->id .'">Сгенерировать QR-КОД<i class="fas fa-qrcode pl-2"></i></a>' .
-                                Html::a('Загрузить изображение<i class="fas fa-file-upload pl-2"></i>', ['/qr/upload-qr', 'id' => $model->id], ['class' => 'btn btn-success', 'style' => 'margin-left: 20px;']) .
-                               '</div>';
-                    }
-                }
-            ],
-            [
-                'attribute' => 'client_id',
-                'format' => 'raw',
-                'value' => function ($model) {
-                    return '<a data-pjax="0" target="_blank" href="/client/view?id=' . $model->client_id . '">' . $model->client_id . '</a>';
-                }
-            ],
-            'first_name',
-            'last_name',
-            'patronymic_name',
-            [
-                'attribute' => 'bdate',
-                'format' => 'html',
-                'value' => function ($model) {
-                    return ($model->bdate) ? Carbon::parse($model->bdate)->format('d.m.Y') : '-';
-                }
-            ],
-            [
-                'attribute' => 'date_death',
-                'format' => 'html',
-                'value' => function ($model) {
-                    return ($model->date_death) ? Carbon::parse($model->date_death)->format('d.m.Y') : '-';
-                }
-            ],
-            'cause_of_death',
-            [
-                'attribute' => 'country_born_id',
-                'format' => 'html',
-                'value' => function ($model) {
-                    return $model->getQrCountryOfBirthName();
-                }
-            ],
-            [
-                'attribute' => 'city_born_id',
-                'format' => 'html',
-                'value' => function ($model) {
-                    return $model->getQrCityOfBirthName();
-                }
-            ],
-            'hobby',
-            'profession',
-            'biography:ntext',
-            'characteristic:ntext',
-            'last_wish:ntext',
-            'comment:ntext',
-            [
-                'attribute' => 'profile_status_id',
-                'format' => 'html',
-                'value' => function ($model) {
-                    return $model->getProfileQrStatusName() . '<i class="fas fa-star pl-2" aria-hidden="true" style="color:' . $model->profileQrStatus->color . ';"></i>';
-                }
-            ],
-            [
-                'attribute' => 'geolocation',
-                'format' => 'raw',
-                'value' => function ($model) {
-                    return ($model->geolocation) ?
-                        '<a target="_blank" data-pjax="0" href="https://www.google.com.ua/maps/search/' . $model->geolocation . '"><i class="fas fa-map-marker-alt" style="color: #ff7675"></i> ' . $model->geolocation . '</a>'
-                        : '-';
-                }
-            ],
-            'slider_img_link',
-            'photo_link',
-            'document_link',
-            'other_link',
-            [
-                'attribute' => 'favourite_song',
-                'format' => 'raw',
-                'value' => function ($model) {
-                    return ($model->favourite_song) ?
-                        '<a target="_blank" data-pjax="0" href="https://music.youtube.com/search?q=' . $model->favourite_song . '"><i class="fab fa-youtube" style="color: #FF0000"></i> ' . $model->favourite_song . '</a>' . '</br>' .
-                        '<a target="_blank" data-pjax="0" href="https://open.spotify.com/search/' . $model->favourite_song . '"><i class="fab fa-spotify" style="color: #1DB954; background-color: #000000"></i> ' . $model->favourite_song . '</a>'
-                        : '-';
-                }
-            ],
-            [
-                'attribute' => 'voice_message',
-                'format' => 'raw',
-                'value' => function ($model) {
-                    return ($model->voice_message) ?
-//                        "<audio controls>
-//                            <source src='" . Yii::getAlias('@web') . "audio/qr-voice-messages/" . $model->voice_message . "' type='mp3'>
-//                         </audio>"
-                        $model->voice_message
-                        : '-';
-                }
-            ],
-        ],
-    ]) ?>
+<!-- row 1  -----------------------------------------------------------------------  -->
+    <div class="row">
+        <div class="col-lg-4">
+            <div class="card" style="border: solid 2px #00759C; background-color: #f5f5f5; height:100%;">
+                <div class="card-body">
+                    <div class="d-flex flex-column align-items-center text-center">
+                        <?php
+                            if (!empty($model->qr_link)) {
+                                echo Html::img(Yii::getAlias('@web') . '/images/qr-codes/' . $model->qr_link, ['width' => '350px',]);
+                            } else {
+                                echo '<div style="display:flex; align-items:center;">' .
+                                     '<a class="btn btn-warning" data-pjax="0" target="_blank" href="https://api.qrserver.com/v1/create-qr-code/?size=800x800&format=svg&data=http://crm_project/qr/profile?id='. $model->id .'">Сгенерировать QR-КОД<i class="fas fa-qrcode pl-2"></i></a>' .
+                                        Html::a('Загрузить изображение<i class="fas fa-file-upload pl-2"></i>', ['/qr/upload-qr', 'id' => $model->id], ['class' => 'btn btn-success', 'style' => 'margin-left: 20px;']) .
+                                     '</div>';
+                            }
+                        ?>
+                    </div>
+                    <hr class="mt-4">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h6 class="mb-0">
+                                <b><?= $model->getAttributeLabel('profile_status_id') ?> :</b>
+                            </h6>
+                        </div>
+                       <div class="col-sm-6 text-center">
+                           <?= $model->profileQrStatus->name ?><i class="fas fa-star pl-2" aria-hidden="true" style="color: <?= $model->profileQrStatus->color ?>"></i>
+                       </div>
+                    </div><hr>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h6 class="mb-0">
+                                <b><?= $model->getAttributeLabel('geolocation') ?> :</b>
+                            </h6>
+                        </div>
+                       <div class="col-sm-6 text-center">
+                           <?php if (!empty($model->geolocation)) { ?>
+                                <a target="_blank" data-pjax="0" href="https://www.google.com.ua/maps/search/<?=$model->geolocation?>"><i class="fas fa-map-marker-alt" style="color: #ff7675; margin-right: 5px;"></i><?=$model->geolocation?></a>
+                           <?php } else {
+                                echo '-';
+                           }
+                           ?>
+                       </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h6 class="mb-0">
+                                <b><?= $model->getAttributeLabel('comment') ?> :</b>
+                            </h6>
+                        </div>
+                        <div class="col-sm-6 text-center">
+                            <?= ($model->comment) ? $model->comment : '-' ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-8">
+            <div class="card" style="border: solid 2px #00759C; background-color: #f5f5f5">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <h6 class="mb-0">
+                                <b>Ф.И.О</b>
+                            </h6>
+                        </div>
+                        <div class="col-sm-8 text-secondary">
+                            <?= $model->getQrName() ?>
+                        </div>
+                    </div><hr>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <h6 class="mb-0">
+                                <b><?= $model->getAttributeLabel('bdate') ?></b>
+                            </h6>
+                        </div>
+                        <div class="col-sm-8 text-secondary">
+                            <?= ($model->bdate) ? Carbon::parse($model->bdate)->format('d.m.Y') : '-' ?>
+                        </div>
+                    </div><hr>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <h6 class="mb-0">
+                                <b><?= $model->getAttributeLabel('date_death') ?></b>
+                            </h6>
+                        </div>
+                        <div class="col-sm-8 text-secondary">
+                            <?= ($model->date_death) ? Carbon::parse($model->date_death)->format('d.m.Y') : '-' ?>
+                        </div>
+                    </div><hr>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <h6 class="mb-0">
+                                <b><?= $model->getAttributeLabel('country_born_id') ?></b>
+                            </h6>
+                        </div>
+                        <div class="col-sm-8 text-secondary">
+                            <?= ($model->country_born_id) ? $model->getQrCountryOfBirthName() : '-' ?>
+                        </div>
+                    </div><hr>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <h6 class="mb-0">
+                                <b><?= $model->getAttributeLabel('city_born_id') ?></b>
+                            </h6>
+                        </div>
+                        <div class="col-sm-8 text-secondary">
+                            <?= ($model->city_born_id) ? $model->getQrCityOfBirthName() : '-' ?>
+                        </div>
+                    </div><hr>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <h6 class="mb-0">
+                                <b><?= $model->getAttributeLabel('profession') ?></b>
+                            </h6>
+                        </div>
+                        <div class="col-sm-8 text-secondary">
+                            <?= ($model->profession) ? $model->profession : '-' ?>
+                        </div>
+                    </div><hr>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <h6 class="mb-0">
+                                <b><?= $model->getAttributeLabel('hobby') ?></b>
+                            </h6>
+                        </div>
+                        <div class="col-sm-8 text-secondary">
+                            <?= ($model->hobby) ? $model->hobby : '-' ?>
+                        </div>
+                    </div><hr>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <h6 class="mb-0">
+                                <b><?= $model->getAttributeLabel('last_wish') ?></b>
+                            </h6>
+                        </div>
+                        <div class="col-sm-8 text-secondary">
+                            <?= ($model->last_wish) ? $model->last_wish : '-' ?>
+                        </div>
+                    </div><hr>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <h6 class="mb-0">
+                                <b><?= $model->getAttributeLabel('favourite_song') ?></b>
+                            </h6>
+                        </div>
+                        <div class="col-sm-8 text-secondary">
+                            <?= ($model->last_wish) ? $model->favourite_song : '-' ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<!-- row 2  -----------------------------------------------------------------------  -->
+    <div class="row" style="margin-top: 20px; padding: 0 15px;">
+        <div class="col-lg-12" style="margin-top: 10px; border: solid 2px #00759C; background-color: #f5f5f5; border-radius: 0.25rem;">
+            <ul class="nav nav-tabs" id="myTab" role="tablist" style="margin-top: 10px;">
+                <li class="nav-item waves-effect waves-light">
+                    <a class="nav-link active" id="biography-tab" data-toggle="tab" href="#biography" role="tab" aria-controls="biography" aria-selected="false">Биография</a>
+                </li>
+                <li class="nav-item waves-effect waves-light">
+                    <a class="nav-link" id="characteristic-tab" data-toggle="tab" href="#characteristic" role="tab" aria-controls="characteristic" aria-selected="false">Характеристика</a>
+                </li>
+                <li class="nav-item waves-effect waves-light">
+                    <a class="nav-link" id="slider-tab" data-toggle="tab" href="#slider" role="tab" aria-controls="slider" aria-selected="true">Слайдер</a>
+                </li>
+                <li class="nav-item waves-effect waves-light">
+                    <a class="nav-link" id="document-tab" data-toggle="tab" href="#document" role="tab" aria-controls="document" aria-selected="true">Документы</a>
+                </li>
+            </ul>
+            <div class="tab-content" id="myTabContent" style="margin-top: 10px; margin-bottom: 10px;">
+                <div class="tab-pane fade active show text-justify" id="biography" role="tabpanel" aria-labelledby="biography-tab">
+                    <?= ($model->biography) ? $model->biography : '-' ?>
+                </div>
+                <div class="tab-pane fade text-justify" id="characteristic" role="tabpanel" aria-labelledby="characteristic-tab">
+                    <?= ($model->characteristic) ? $model->characteristic : '-' ?>
+                </div>
+                <div class="tab-pane fade" id="slider" role="tabpanel" aria-labelledby="slider-tab">
+                    <?= $this->render('/qr/view-tabs/tab_sliders', [
+                            'model' => $model,
+                        ]);
+                    ?>
+                </div>
+                <div class="tab-pane fade" id="document" role="tabpanel" aria-labelledby="document-tab">Etsy mixtape wayfarers,
+                    ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi
+                    farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard locavore
+                    carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify squid 8-bit cred
+                    pitchfork. Williamsburg banh mi whatever gluten-free, carles pitchfork biodiesel fixie etsy retro mlkshk
+                    vice blog. Scenester cred you probably haven't heard of them, vinyl craft beer blog stumptown. Pitchfork
+                    sustainable tofu synth chambray yr.
+                </div>
+            </div>
+        </div>
+    </div>
 
 </div>
